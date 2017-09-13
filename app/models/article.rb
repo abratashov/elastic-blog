b27599b243f82325def7432ed3c7a30037bc8515
Oleksandr Bratashov <abratashov@gmail.com>
2017-09-14 01:31:01 +0300

    added elasticsearch for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+require 'elasticsearch/model'
+
 class Article < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+Article.import force: true# for auto sync model with elastic search
